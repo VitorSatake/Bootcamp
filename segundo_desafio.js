dc15edbf4022b32ca25f5f267201eb04fa0eaa16
vitor <vitor.scastro2013@hotmail.com>
2022-12-07 12:02:37 -0300

    Segundo desafio

---
+++ segundo_desafio.js
@@ -0,0 +1,12 @@
+/*Faça um programa para calcular o valor gasto de combustível em uma viagem.
+
+Você terá 5 variáveis. Sendo ela:
+
+- 1 Preço do do etanol;
+- 2 Preço da gasolina;
+- 3 O tipo de combustível que está no seu carro;
+- 4 Gasto médio de combustível do carro por KM;
+- 5 Distância em KM da viagem;
+
+Imprima no console o valor que será gasto para realizar essa viagem.
+*/
