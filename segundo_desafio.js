a90ede25cb6f4ee970e6ea25c3c37b057baad3ea
vitor <vitor.scastro2013@hotmail.com>
2022-12-07 12:27:35 -0300

    Conclusão segundo desafio

--- segundo_desafio.js
+++ segundo_desafio.js
@@ -10,3 +10,21 @@ Você terá 5 variáveis. Sendo ela:
 
 Imprima no console o valor que será gasto para realizar essa viagem.
 */
+
+const precoEtanol = 5.79;
+const precoGasolina = 7.89;
+const tipoCombustivel = 'Etanol';
+const Km_por_litro = 7;
+const distanciaViagemKm = 100; 
+
+const valorGastoGasolina = (distanciaViagemKm/Km_por_litro)*precoGasolina;
+const valorGastoEtanol = (distanciaViagemKm/Km_por_litro)*precoEtanol;
+
+if (tipoCombustivel === 'Gasolina') {
+    console.log('O valor gasto nessa viagem utizando Gasolina é de : R$ ' + valorGastoGasolina.toFixed(2) + '.'); //tofixed() casas decimais
+} else if (tipoCombustivel === 'Etanol') {
+    console.log('O valor gasto nessa viagem utizando Etanol é de : R$  ' + valorGastoEtanol.toFixed(2) + '.'); //tofixed() casas decimais
+} else {
+    console.log('Insira um tipo de combustível válido, Etanol ou Gasolina !');
+}
+
