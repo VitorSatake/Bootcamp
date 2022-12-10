5e53cfdef3aba694afbdaaf18a58ddb353ab73d6
vitor <vitor.scastro2013@hotmail.com>
2022-12-10 17:15:00 -0300

    Enviando exercicio

--- JavaScript/funcoes_auxiliares.js
+++ JavaScript/funcoes_auxiliares.js
@@ -1,8 +1,14 @@
 
 // Exportação das funçoes
 
+
+const entradas = [5, 50, 10, 98, 23];
+let i = 0;
+
 function gets() {
-        return 10;
+    const valor = entradas[i];
+    i = i + 1; // ou i++
+    return valor;
 }
 
 
--- JavaScript/main_funcoes_auxiliares.js
+++ JavaScript/main_funcoes_auxiliares.js
@@ -10,4 +10,44 @@
 // Outra forma de importar
 const { gets, print } = require('./funcoes_auxiliares');
 
-print(gets());
+//print(gets());
+
+// Exercicio 
+
+/* 
+
+Uma sala de aula contem 5 alunos e para cada aluno foi sorteado um numero de 1 - 100.
+Faça um programa que receba os 5 numeros sorteados para os alunos e mostre o maior numero sorteado.
+
+Dados de entrada:
+
+  5;
+  50;
+  10;
+  98;
+  23;
+
+Saída:
+
+  98;
+
+*/
+
+
+const numerosSorteados = [];
+
+for (let i = 0; i < 5; i++) {
+    const numeroSorteado = gets();
+    numerosSorteados.push(numeroSorteado);
+}
+
+let maiorValor = 0;
+
+for (let i = 0; i < numerosSorteados.length; i++) {
+    const numeroSorteado = numerosSorteados[i];
+    if (numeroSorteado > maiorValor) {
+        maiorValor = numeroSorteado;
+    }
+}
+
+print(maiorValor);
