2c0e90baae24670f1a22f84aba4fb33065299c7f
vitor <vitor.scastro2013@hotmail.com>
2022-12-16 16:00:04 -0300

    Enviando atualizações

--- JavaScript/main_pokedex.js
+++ JavaScript/main_pokedex.js
@@ -1,10 +1,31 @@
 
 
-const offset = 0
-const limit = 10
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+function convertPokemonTypesToLi(pokemonTypes) {
+    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
+}
 
-fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => console.log(jsonBody))
-    .catch((error) => console.error(error))
+function convertPokemonToLi(pokemon) {
+    return `
+        <li class="pokemon">
+            <span class="number">#${pokemon.order}</span>
+            <span class="name">${pokemon.name}</span>
+
+            <div class="detail">
+                <ol class="types">
+                    ${convertPokemonTypesToLi(pokemon.types).join(' ')}
+                </ol>
+                <img src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}">
+            </div>                      
+        </li>
+
+
+    `
+}
+
+const pokemonList = document.getElementById('pokemonList')
+
+
+pokeApi.getPokemons().then((pokemons = []) => {   
+    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
+   
+})
