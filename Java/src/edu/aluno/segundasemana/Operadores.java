211044fe82de097a4c26ed4b743f4c19e57a3ddd
vitor <vitor.scastro2013@hotmail.com>
2022-12-27 12:25:26 -0300

    Enviando novas aulas

---
+++ Java/src/edu/aluno/segundasemana/Operadores.java
@@ -0,0 +1,9 @@
+package edu.aluno.segundasemana;
+
+public class Operadores {
+    /* 
+    atribuição : =
+    aritmético : + (adição), - (subtração), * (multiplicação), / (divisão)
+    O operador + em tipo textos, realizará a concatenação dos textos
+    */
+}
---
+++ Java/src/edu/aluno/segundasemana/TiposVariaveis.java
@@ -0,0 +1,6 @@
+package edu.aluno.segundasemana;
+
+public class TiposVariaveis {
+    // tipos primitivos: short, byte, int, long, float, double, boolean, char, 
+    // constante : final double VALOR_PI = 3.14; final antes e em caixa alta
+}
