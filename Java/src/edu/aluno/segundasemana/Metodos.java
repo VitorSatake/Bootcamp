2c6bf889ad69f4bca4502571caaec8355b4d34e7
vitor <vitor.scastro2013@hotmail.com>
2022-12-28 10:37:04 -0300

    Enviando novas aulas

---
+++ Java/src/edu/aluno/segundasemana/Metodos.java
@@ -0,0 +1,23 @@
+package edu.aluno.segundasemana;
+
+public class Metodos {
+    
+
+    /*
+    Deve ser nomeado como verbo
+    Seguir o padrao camelCase (todas as letras minusculoas, menos a primeira da segunda palavra)
+    Ex: somar(int n1, int n2){}
+        abrirConexao {}
+        concluirProcessamento {}
+        findById {}
+        calcularImprimir {} : errado, nome com duas ações
+
+        Nao ha conceito de metodos globais, sempre dentro de uma classe
+
+        Se nao retornar nada , palavra void
+
+
+
+     */
+    
+}
--- Java/src/edu/aluno/segundasemana/Operadores.java
+++ Java/src/edu/aluno/segundasemana/Operadores.java
@@ -1,9 +1,24 @@
 package edu.aluno.segundasemana;
 
 public class Operadores {
+    public static void main(String[] args) {
     /* 
     atribuição : =
     aritmético : + (adição), - (subtração), * (multiplicação), / (divisão)
     O operador + em tipo textos, realizará a concatenação dos textos
+
+    Operador ternario : mesmo que um if basicamente, porem escrito em uma linha :
+    Sempre a primeira expressaao é booleano, a == b
+    Da mesma forma invés de retornar uma string pode retornar um int, int resultado = a == b ? 1 : 0;
+    
+    int a, b;
+    a = 6;
+    b = 6;
+    String resultado = a == b ? "verdadeiro" : "falso";
+    System.out.println(resultado);
     */
+
+    
+    }
+
 }
