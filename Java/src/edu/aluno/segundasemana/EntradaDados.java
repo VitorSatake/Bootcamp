076e8269cde9b9a5c49b1c6b4d37309144f891a0
vitor <vitor.scastro2013@hotmail.com>
2022-12-30 10:21:45 -0300

    Enviando nova aula

---
+++ Java/src/edu/aluno/segundasemana/EntradaDados.java
@@ -0,0 +1,41 @@
+package edu.aluno.segundasemana;
+
+public class EntradaDados {
+    /*
+     * import java.util.Locale;
+     * import java.util.Scanner;
+     * 
+     * public class AboutMe {
+     *  public static void main(String[] args) {
+     *    //criando o objeto scanner
+     *    Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+     * 
+     *    System.out.println("Digite sua altura");
+     *      double altura = scanner.nextDouble();
+     * 
+     *      System.out.println("Digite seu nome");
+     *      Sting nome = scanner.next();
+     * 
+     *      System.out.println("Digite seu sobrenome");
+     *      String sobrenome = scanner.next(); 
+     * 
+     *      System.out.println("Digite sua idade");
+     *      int idade = scanner.nextInt();
+     * 
+     *   // Imprimindo os dados obtidos pelo usuario
+     * 
+     *      System.out.println("Ola, me chamo " + nome + " " + sobrenome);
+     *      System.out.println("Tenho " + idade + "anos");
+     *      System.out.println("Minha altura é " + altura + "cm");
+     *
+     *  }
+     * }
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     */
+}
