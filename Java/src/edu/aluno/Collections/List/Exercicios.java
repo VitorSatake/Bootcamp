d2599ab55c229a0af15452ca19abf0927a48ec2c
vitor <vitor.scastro2013@hotmail.com>
2023-01-23 10:10:09 -0300

    Novas aulas

---
+++ Java/src/edu/aluno/Collections/List/Exercicios.java
@@ -0,0 +1,22 @@
+package edu.aluno.Collections.List;
+
+
+/* 1 - Faça um programa que receba a temperatura média dos 6 primeiros meses do ano e armazena-as em uma lista.
+ * Após isto, calcule a média semestral das temperaturas e mostre todas as temperaturas acima da média, e em 
+ * mes elas ocorreram ( mostrar o mes por extenso: 1 - Janeiro, 2 - Fevereiro, etc.)
+ * 
+ * 2 - Utilizando Listas, faça um programa que faça 5 perguntas para uma pessoa sobre um crime.
+ *  As perguntas são:
+ *     1 - "Telefonou para a vitima?"
+ *     2 - "Esteve no local do crime?"
+ *     3 - "Mora perto da vitima?"
+ *     4 - "Devia para a vitima?"
+ *     5 - "Ja trabalhou com a vitima?"
+ * 
+ * Se a pessoa responder positivamente a 2 questoes ela deve ser classificada como "Suspeita", entre 3 e 4 
+ * como "Cumplice" e 5 como "Assassina". Caso contrario, ela sera classificada como "Inocente".
+ * 
+ */
+public class Exercicios {
+    
+}
--- Java/src/edu/aluno/Collections/List/Lista.java
+++ Java/src/edu/aluno/Collections/List/Lista.java
@@ -3,6 +3,8 @@ package edu.aluno.Collections.List;
 // Dada uma lista com 7 notas de um aluno [7, 8.5, 9.3, 5, 7, 0, 3.6], faça :
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
 class Lista {
@@ -35,7 +37,45 @@ class Lista {
         System.out.println("Exiba todas as notas na ordem que foram informados: ");
         for (Double nota : notas) System.out.println(nota);
 
-        System.out.println("Exiba a terceira nota adicionada: ");
+        System.out.println("Exiba a terceira nota adicionada: " + notas.get(2));
+
+        System.out.println("Exiba a menor nota: " + Collections.min(notas));
+
+        System.out.println("Exiba a maior nota: " + Collections.max(notas));
+
+        Iterator<Double> iterator = notas.iterator();
+        Double soma = 0d;
+        while(iterator.hasNext()){
+            Double next = iterator.next();
+            soma += next;
+        }
+
+        System.out.println("Exiba a soma dos valores: " + soma);
+
+        System.out.println("Exiba a média das notas: " + (soma/notas.size()));
+
+        System.out.println("Remova a nota 0: ");
+        notas.remove(0d); // colocar o d para saber que é o valor 0 e nao posição 0
+        System.out.println(notas);
+
+        System.out.println("Remova a nota na posição 0: ");
+        notas.remove(0); 
+        System.out.println(notas);
+
+        System.out.println("Remova as notas menores que 7 e exiba a lista: ");
+        Iterator<Double> iterator1 = notas.iterator();
+        while(iterator1.hasNext()){
+            Double next = iterator1.next();
+            if(next < 7) iterator1.remove();
+        }
+        System.out.println(notas);
+
+        System.out.println("Apague toda a lista: ");
+        notas.clear();
+        System.out.println(notas);
+
+        System.out.println("Confira se a lista está vazia: " + notas.isEmpty()); // retorna true or false para saber se está vazia
+
 
     }
 }
